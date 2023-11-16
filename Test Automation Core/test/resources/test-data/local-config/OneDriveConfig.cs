cc42a78dfbc0dc3c8cb4694e3aec7bb807023bb6
Yassine Mahfoudh <yassine.mahfoudh@atlasti.com>
2023-11-16 12:23:54 +0100

    -

--- Test Automation Core/test/resources/test-data/local-config/OneDriveConfig.cs
+++ Test Automation Core/test/resources/test-data/local-config/OneDriveConfig.cs
@@ -8,7 +8,7 @@ namespace Test_Automation_Core.test.resources.test
         //Get the one drive path automatically 
         public static string testingStuffPath = SystemActions.GetOneDrivePath() + "\\Testing stuff";
 
-        //If you need to set the one drive path manually and convert the line above to a comment, and the line below to code.
+        //If you need to set the one drive path manually , convert the line above to a comment, and the line below to code.
 
          //public static string testingStuffPath =  @"\\Mac\Home\Library\CloudStorage\OneDrive-ATLAS.tiScientificSoftwareDevelopmentGmbH\Testing stuff";
 
